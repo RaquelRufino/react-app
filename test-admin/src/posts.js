e9cde54145dcdb186e85fa3fad9b3b7454cf91c4
RaquelRufino <quelrufino35@gmail.com>
2020-03-02 15:04:10 -0300

    done the app

---
+++ test-admin/src/posts.js
@@ -0,0 +1,84 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { useMediaQuery } from "@material-ui/core";
+import {
+  List,
+  Datagrid,
+  TextField,
+  ReferenceField,
+  EditButton,
+  SimpleForm,
+  ReferenceInput,
+  SelectInput,
+  TextInput,
+  Create,
+  Edit,
+  Filter,
+  SimpleList
+} from "react-admin";
+
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ""}</span>;
+};
+
+const PostFilter = props => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
+);
+
+export const PostList = props => {
+  const isSmall = useMediaQuery(theme => theme.breakpoints.down("sm"));
+  return (
+    <List {...props}>
+      {isSmall ? (
+        <SimpleList
+          primaryText={record => record.title}
+          secondaryText={record => `${record.views} views`}
+          tertiaryText={record =>
+            new Date(record.published_at).toLocaleDateString()
+          }
+        />
+      ) : (
+        <Datagrid>
+          <TextField source="id" />
+          <ReferenceField label="User" source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField source="title" />
+          <TextField source="body" />
+          <EditButton />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
+
+export const PostEdit = props => (
+  <Edit title={<PostTitle />} {...props}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Edit>
+);
+
+export const PostCreate = props => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Create>
+);
